88a466c5ffc67e1bfd8dfecebcdf4fbadd76686d
nitinkanazariya <128599011+nitinkanazariya@users.noreply.github.com>
2025-01-07 19:33:55 +0530

    Create home.js

---
+++ Src/Screen/home/home.js
@@ -0,0 +1,25 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize'
+import { HEIGHT } from '../../Component/Constant'
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+
+
+const Home = () => {
+  // console.log(RFValue(10));
+
+  return (
+    <View style={{ alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+      <Text style={{ textAlign: 'centers', }}>Home</Text >
+      {/* <Text style={{ textAlign: 'centers', fontSize: RFValue(15, HEIGHT) }}>Home</Text > */}
+      {/* <Text style={{ textAlign: 'centers', fontSize: RFPercentage(10) }}>Home</Text > */}
+      {/* <Text style={{ textAlign: 'centers', fontSize: hp('5%') }}>H ome</Text > */}
+    </View>
+  )
+}
+
+export default Home
+
+
+// RFValue(24, 580)
+// RFPercentage(5)
