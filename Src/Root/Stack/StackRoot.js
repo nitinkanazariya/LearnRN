bc5e6c8203a846d5a2882e00b6f29fb2b95f40fa
nitinkanazariya <128599011+nitinkanazariya@users.noreply.github.com>
2025-01-08 19:15:53 +0530

    Update StackRoot.js

--- Src/Root/Stack/StackRoot.js
+++ Src/Root/Stack/StackRoot.js
@@ -3,6 +3,8 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Home from '../../Screen/home/home'
 import Login from '../../Screen/login/login'
+import ExtraData from '../../Screen/FlatList/ExtraData'
+import { RootName } from '../../Constent/OBJ'
 
 
 const Stack = createNativeStackNavigator()
@@ -10,9 +12,12 @@ const Stack = createNativeStackNavigator()
 const StackRoot = () => {
   return (
     <NavigationContainer >
-      <Stack.Navigator screenOptions={{ headerShown: false, animation: 'fade' }}>
-        <Stack.Screen name='home' component={Home} />
-        <Stack.Screen name='login' component={Login} />
+      <Stack.Navigator  screenOptions={{ headerShown: false, animation: 'fade' }}>
+
+        <Stack.Screen name={RootName.home} component={Home} />
+        <Stack.Screen name={RootName.login} component={Login} />
+        <Stack.Screen name={RootName.extradata} component={ExtraData} />
+        
       </Stack.Navigator>
     </NavigationContainer>
   )
