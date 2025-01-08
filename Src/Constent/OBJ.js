2f488e160abb79acf05ed344b07b87ea3e887f92
nitinkanazariya <128599011+nitinkanazariya@users.noreply.github.com>
2025-01-08 19:15:55 +0530

    Create OBJ.js

---
+++ Src/Constent/OBJ.js
@@ -0,0 +1,12 @@
+export const RootName = {
+  home: 'home',
+  login: 'login',
+  extradata: 'extradata',
+
+}
+
+export const ListData = [
+  { id: 1, name: 'Home', navigation: RootName.home },
+  { id: 2, name: 'Login', navigation: RootName.login },
+  { id: 3, name: 'FlatList ExtraData', navigation: RootName.extradata },
+]
