d5bb22f84c7606c176905d78c83d10957c701282
nitinkanazariya <128599011+nitinkanazariya@users.noreply.github.com>
2025-01-08 19:15:57 +0530

    Create ExtraData.js

---
+++ Src/Screen/FlatList/ExtraData.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ExtraData = () => {
+  return (
+    <View>
+      <Text>ExtraData</Text>
+    </View>
+  )
+}
+
+export default ExtraData
