8815d87cac4d23264e33f4b67cda800015f22953
nitinkanazariya <128599011+nitinkanazariya@users.noreply.github.com>
2025-01-08 11:08:53 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import Home from './Src/Screen/home/home'
+import Config from 'react-native-config'
 
 const App = () => {
+  console.log('env============================================>>>', Config.API_URL)
   return (
     <Home />
   )
