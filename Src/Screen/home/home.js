44bad9337875e48cf9049d16e9881fc4b87d3c31
nitinkanazariya <128599011+nitinkanazariya@users.noreply.github.com>
2025-01-09 12:13:30 +0530

    Update home.js

--- Src/Screen/home/home.js
+++ Src/Screen/home/home.js
@@ -2,16 +2,16 @@ import { View, Text, FlatList, StatusBar, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { ListData } from '../../Constent/OBJ'
 import { RFValue } from 'react-native-responsive-fontsize'
-import { HEIGHT } from '../../Component/Constant'
+
 
 
 const Home = (props) => {
 
   const list = ({ item, index }) => {
     return (
-      <TouchableOpacity 
-      onPress={()=>{props.navigation.navigate(item.navigation)}}
-      style={{ backgroundColor: 'white', flexDirection: 'row', padding: 10, margin: 5, borderRadius: 10, alignItems: 'center' }}>
+      <TouchableOpacity
+        onPress={() => { props.navigation.navigate(item.navigation) }}
+        style={{ backgroundColor: 'white', flexDirection: 'row', padding: 10, margin: 5, borderRadius: 10, alignItems: 'center' }}>
         <Text style={{ fontSize: RFValue(16), marginRight: 10, fontWeight: 'bold' }}>{`${index + 1}]`}</Text>
         <Text style={{ fontSize: RFValue(15), fontWeight: '500' }}>{item.name}</Text>
       </TouchableOpacity>
@@ -20,6 +20,7 @@ const Home = (props) => {
   return (
     <View style={{ flex: 1, padding: 10, backgroundColor: 'black' }}>
       <StatusBar backgroundColor={'black'} />
+
       <Text style={{ fontSize: RFValue(20), color: 'white', margin: 20, textAlign: 'center', fontWeight: 'bold' }}>All Concept</Text>
       <FlatList data={ListData} renderItem={list} />
 
