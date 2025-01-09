d408e9640b9787aa220e02ae638ecb9aa964a662
nitinkanazariya <128599011+nitinkanazariya@users.noreply.github.com>
2025-01-09 19:23:02 +0530

    Update Product.js

--- Src/Screen/Products/Product.js
+++ Src/Screen/Products/Product.js
@@ -3,13 +3,13 @@ import React from 'react'
 import { HEIGHT, WIDTH } from '../../Component/Constant';
 
 const Product = (props) => {
-  const { productId } = props.route.params
+  const productId = props?.route?.params?.productId
   console.log(productId);
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       {/* <Image source={{ uri: productId }} style={{ height: HEIGHT, width: WIDTH, resizeMode: 'contain' }} /> */}
-      <Text>{productId}</Text>
+      <Text style={{ fontSize: 20, fontWeight: '500' }}>{productId ? productId : 'Not found'}</Text>
     </View>
   )
 }
