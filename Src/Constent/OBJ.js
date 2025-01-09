40eb669d7d0ea5787b4b0782397107a9290d8501
nitinkanazariya <128599011+nitinkanazariya@users.noreply.github.com>
2025-01-09 18:43:46 +0530

    Update OBJ.js

--- Src/Constent/OBJ.js
+++ Src/Constent/OBJ.js
@@ -2,6 +2,7 @@ export const RootName = {
   home: 'home',
   login: 'login',
   extradata: 'extradata',
+  product: 'product',
 
 }
 
@@ -9,4 +10,5 @@ export const ListData = [
   { id: 1, name: 'Home', navigation: RootName.home },
   { id: 2, name: 'Login', navigation: RootName.login },
   { id: 3, name: 'FlatList ExtraData', navigation: RootName.extradata },
+  { id: 4, name: 'Product', navigation: RootName.product },
 ]
