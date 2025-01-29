5fb0d5c46d30b4376e740901d34520f9079b4c67
nitinkanazariya <128599011+nitinkanazariya@users.noreply.github.com>
2025-01-29 11:50:59 +0530

    Update OBJ.js

--- Src/Constent/OBJ.js
+++ Src/Constent/OBJ.js
@@ -1,6 +1,7 @@
 export const RootName = {
   home: 'home',
   login: 'login',
+  signUp: 'signup',
   extradata: 'extradata',
   product: 'product',
   autofillOtp: 'autofillotp',
@@ -8,16 +9,16 @@ export const RootName = {
   skeleton: 'skeleton',
   sectionlist: 'sectionlist',
   socketio: 'socketio',
+  chatuserlist: 'chatuserlist'
 
 }
 
 export const ListData = [
   { id: 1, name: 'Home', navigation: RootName.home },
-  { id: 2, name: 'Login', navigation: RootName.login },
-  { id: 3, name: 'FlatList ExtraData', navigation: RootName.extradata },
-  { id: 4, name: 'DeepLinking', navigation: RootName.product },
-  { id: 5, name: 'Auto Fill Otp', navigation: RootName.autofillOtp },
-  { id: 6, name: 'Skeleton loader', navigation: RootName.skeleton },
-  { id: 7, name: 'Sectionlist Ex. ', navigation: RootName.sectionlist },
-  { id: 8, name: 'Chat With Socket', navigation: RootName.socketio },
+  { id: 2, name: 'FlatList ExtraData', navigation: RootName.extradata },
+  { id: 3, name: 'DeepLinking', navigation: RootName.product },
+  { id: 4, name: 'Auto Fill Otp', navigation: RootName.autofillOtp },
+  { id: 5, name: 'Skeleton loader', navigation: RootName.skeleton },
+  { id: 6, name: 'Sectionlist Ex. ', navigation: RootName.sectionlist },
+  { id: 7, name: 'Chat With Socket', navigation: RootName.login },
 ]
