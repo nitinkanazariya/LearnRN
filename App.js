695a4d04af70e805a2fdfd8d99fc4851af7de6eb
nitinkanazariya <nitinkanazariya61506@gmail.com>
2025-02-25 16:25:55 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,18 +1,33 @@
-import { View, Text, LogBox } from 'react-native'
+// import { View, Text, LogBox } from 'react-native'
+// import React from 'react'
+// import StackRoot from './Src/Root/Stack/StackRoot'
+// import { StoreProvider } from './Src/service/contextStore'
+// LogBox.ignoreAllLogs()
+// const App = () => {
+
+
+//   return (
+//     <>
+//       <StoreProvider>
+//         <StackRoot />
+//       </StoreProvider>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+import { View, Text, TextInput } from 'react-native'
 import React from 'react'
-import Config from 'react-native-config'
-import StackRoot from './Src/Root/Stack/StackRoot'
-LogBox.ignoreAllLogs()
+import CustomTextInput from './Src/Component/CustomTextInput'
+
 const App = () => {
-
-  console.log(Config.API_URL);
-
   return (
-    <>
-      <StackRoot />
-    </>
+    <View style={{ backgroundColor: 'black', flex: 1 }}>
+      <CustomTextInput ContainerStyle={{ marginTop: 50, }} LeftIcon={'https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-locked-512.png'} />
+    </View>
   )
 }
 
 export default App
-
